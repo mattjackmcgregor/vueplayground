3055bc451d7aeb540b90b465f7bc477eb62f93cc
mattjackmcgregor <mattjackmcgregor@gmail.com>
2018-12-03 09:05:15 +1300

    learning vue

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'socks',
+    description: "banging ass socks",
+    image: './src/green-onWhite.jpg',
+    inventory: 15,
+    sizes: [2,3,4,5,6,7,8,9,10],
+  }
+})
